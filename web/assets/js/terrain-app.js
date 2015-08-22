c98b9e33568e51de671f90067fbf50632de8f4ea
Mike Agnich <mike@terraindb.com>
2015-08-21 17:22:45 -0700

    Updated Date and Time pickers in inputs

--- web/assets/js/terrain-app.js
+++ web/assets/js/terrain-app.js
@@ -142,6 +142,33 @@ terrainApp.directive('ngRightClickMenu', function($parse) {
     };
 });
 
+terrainApp.directive('moDateInput', function ($window) {
+    return {
+        require:'^ngModel',
+        restrict:'A',
+        link:function (scope, elm, attrs, ctrl) {
+            var moment = $window.moment;
+            var dateFormat = attrs.moMediumDate;
+            attrs.$observe('moDateInput', function (newValue) {
+                if (dateFormat == newValue || !ctrl.$modelValue) return;
+                dateFormat = newValue;
+                ctrl.$modelValue = new Date(ctrl.$setViewValue);
+            });
+
+            ctrl.$formatters.unshift(function (modelValue) {
+                if (!dateFormat || !modelValue) return "";
+                var retVal = moment(modelValue).format(dateFormat);
+                return retVal;
+            });
+
+            ctrl.$parsers.unshift(function (viewValue) {
+                var date = moment(viewValue, dateFormat);
+                return (date && date.isValid() && date.year() > 1950 ) ? date.toDate() : "";
+            });
+        }
+    };
+});
+
 terrainApp.directive('ngStepChange', function($parse) {
 	return function(scope, element, attrs) {
         var fn = $parse(attrs.ngStepChange);
