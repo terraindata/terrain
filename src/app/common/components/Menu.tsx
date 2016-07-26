40a24fb757485b89ec92ca6d7e5db02c3e21f4a9
lbrouckman <laura.brouckman@gmail.com>
2016-07-25 17:29:20 -0700

    Minor changes to menu styling

--- src/app/common/components/Menu.tsx
+++ src/app/common/components/Menu.tsx
@@ -102,7 +102,9 @@ class Menu extends Classs<Props>
           }}>
           {option.icon}
         </div>
-        <div className="menu-text-padding">
+        <div 
+          className={option.icon ? "menu-text-padding" : "menu-text-padding-no-icon"}
+        >
           { option.text }
         </div>
       </div>
@@ -141,8 +143,9 @@ class Menu extends Classs<Props>
       return null;
     }
     
-    var width = 14 * options.reduce((max, option) => 
+    var width = 12 * options.reduce((max, option) => 
         option.text.length > max ? option.text.length : max, 1)
+
     var style = {
       width: width,
       height: options.length * optionHeight,
