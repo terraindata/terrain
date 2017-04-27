91ad30c062c0cf5ae48689344ba1a0c1ccf1e710
adk9 <abd.kulkarni@gmail.com>
2017-04-26 17:12:42 -0700

    Import auth/Passport.ts.

--- midway/src/App.ts
+++ midway/src/App.ts
@@ -54,6 +54,7 @@ import dateFormat = require('date-format');
 import reqText = require('require-text');
 import session = require('koa-generic-session');
 
+import './auth/Passport.ts';
 import DB from './DB';
 import Middleware from './Middleware';
 import Router from './Router';
