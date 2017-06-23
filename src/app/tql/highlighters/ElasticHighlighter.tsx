090ac26458d0d4537e003183ebab5ca17bb7ebfb
aleiux <alexliu314@gmail.com>
2017-06-23 14:58:09 -0700

    forgot to add file

--- src/app/tql/highlighters/ElasticHighlighter.tsx
+++ src/app/tql/highlighters/ElasticHighlighter.tsx
@@ -44,22 +44,95 @@ THE SOFTWARE.
 
 // Copyright 2017 Terrain Data, Inc.
 import ESJSONParser from '../../../../shared/backends/elastic/parser/ESJSONParser';
+import ESJSONType from '../../../../shared/backends/elastic/parser/ESJSONType';
 import ESParserToken from '../../../../shared/backends/elastic/parser/ESParserToken';
+import ESValueInfo from '../../../../shared/backends/elastic/parser/ESValueInfo';
 import SyntaxHighlighter from './SyntaxHighlighter';
+
+type TextCoordinates = [{ line: number, ch: number }, { line: number, ch: number }];
 
 class ElasticHighlighter extends SyntaxHighlighter
 {
   protected markers: any[];
 
+  constructor()
+  {
+    super();
+    this.markers = [];
+  }
+
   /*
-   *  handle the 'changes' event emitted by CodeMirror
+   *  Handle the 'changes' event emitted by CodeMirror
    */
   public handleChanges(instance, changes: object[]): void
   {
     // const marker = instance.markText({line: 0, ch: 0}, {line: 1, ch: 1}, {className: "cm-number"});
     // tslint:disable-next-line no-console
+    this.clearMarkers();
     const parser = new ESJSONParser(instance.getValue());
     const tokens: ESParserToken[] = parser.getTokens();
+    for (let i = 0; i < tokens.length; i++)
+    {
+      if (tokens[i].valueInfo)
+      {
+        const valueInfo: ESValueInfo = tokens[i].valueInfo;
+        let style: string = '';
+        switch (valueInfo.jsonType)
+        {
+          case ESJSONType.invalid:
+            style = 'cm-error';
+            break;
+          case ESJSONType.null:
+          case ESJSONType.boolean:
+          case ESJSONType.number:
+            style = 'cm-number';
+            break;
+          case ESJSONType.string:
+            style = 'cm-string';
+            break;
+          case ESJSONType.array:
+          case ESJSONType.object:
+            style = 'cm-bracket';
+            break;
+          case ESJSONType.parameter:
+            style = 'cm-meta';
+            break;
+        }
+        const coords: TextCoordinates = this.getTokenCoordinates(tokens[i]);
+        const marker = instance.markText(coords[0], coords[1], { className: style });
+        // tslint:disable-next-line no-console
+        // console.log([coords[0], coords[1], {className: style}]);
+        console.log(style);
+        this.markers.push(marker);
+      }
+    }
+  }
+
+  protected clearMarkers(): void
+  {
+    for (let i = 0; i < this.markers.length; i++)
+    {
+      this.markers[i].clear();
+    }
+    this.markers = [];
+  }
+
+  protected getTokenCoordinates(token: ESParserToken): TextCoordinates
+  {
+    const row0 = token.row + 1;
+    const col0 = token.col;
+    const lines = token.substring.replace('\r', '').replace('\n', '').split('\n');
+    const row1 = row0 + lines.length - 1;
+    const col1 = lines.length === 1 ? col0 + token.length : lines[lines.length - 1].length;
+    // tslint:disable-next-line no-console
+    console.log('-------------------');
+    // console.log(token.substring);
+    console.log(lines);
+    console.log(row0, col0);
+    console.log(row1, col1);
+    return [{ line: row0, ch: col1 }, { line: row1, ch: col1 }];
   }
 }
 export default ElasticHighlighter;
+
+/* tslint:disable */
