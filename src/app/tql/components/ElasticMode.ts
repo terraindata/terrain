1406b364efa458fbd3589397ec244a5d225789b4
adk9 <abd.kulkarni@gmail.com>
2017-07-20 14:45:48 -0700

    Define codemirror modes in TS

--- src/app/tql/components/ElasticMode.ts
+++ src/app/tql/components/ElasticMode.ts
@@ -44,38 +44,44 @@ THE SOFTWARE.
 
 // Copyright 2017 Terrain Data, Inc.
 
+import * as CodeMirror from 'codemirror';
 import ESInterpreter from '../../../../shared/database/elastic/parser/ESInterpreter';
 
-(function(mod) {
-  if (typeof exports == "object" && typeof module == "object") // CommonJS
-    mod(require("../../../../node_modules/codemirror/lib/codemirror"));
-  else if (typeof define == "function" && define.amd) // AMD
-    define(["../../../../node_modules/codemirror/lib/codemirror"], mod);
-  else // Plain browser env
-    mod(CodeMirror);
-})(function(CodeMirror) {
-  "use strict";
-
-    CodeMirror.registerHelper("lint", "elastic", function(text)
+CodeMirror.defineMode("elastic", (config, parserConfig) =>
+{
+  return {
+    startState: () =>
     {
-      var found = [];
-      try
-      {
-        const t = new ESInterpreter(text);
-        for (let e of t.parser.errors)
-        {
-          const token = e.token;
-          found.push({
-            from: CodeMirror.Pos(token.row, token.col),
-            to: CodeMirror.Pos(token.toRow, token.toCol),
-            message: e.message
-          });
-        }
-      }
-      catch(e)
-      {
-        console.log('Exception when parsing ' + text + " error: " + e);
-      }
-      return found;
-  });
+      return {}
+    },
+    token: (stream, state) =>
+    {
+      stream.skipToEnd();
+      return null;
+    },
+    helperType: "elastic"
+  };
+});
+
+CodeMirror.registerHelper("lint", "elastic", (text) =>
+{
+  const found = [];
+  try
+  {
+    const t = new ESInterpreter(text);
+    for (const e of t.parser.getErrors())
+    {
+      const token = e.token;
+      found.push({
+        from: CodeMirror.Pos(token.row, token.col),
+        to: CodeMirror.Pos(token.toRow, token.toCol),
+        message: e.message
+      });
+    }
+  }
+  catch (e)
+  {
+    throw new Error('Exception when parsing ' + text + " error: " + e);
+  }
+  return found;
 });
--- src/app/tql/components/TQLEditor.tsx
+++ src/app/tql/components/TQLEditor.tsx
@@ -44,17 +44,16 @@ THE SOFTWARE.
 
 // Copyright 2017 Terrain Data, Inc.
 
-// tslint:disable:no-var-requires strict-boolean-expressions
+// tslint:disable:strict-boolean-expressions
 
 import * as classNames from 'classnames';
-import * as Immutable from 'immutable';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+
+import CodeMirror = require('./Codemirror.js');
 import './TQLEditor.less';
-const { List } = Immutable;
-import * as _ from 'underscore';
+
 import TerrainComponent from './../../common/components/TerrainComponent';
-const CodeMirror = require('./Codemirror.js');
 
 // syntax highlighters
 import ElasticHighlighter from '../highlighters/ElasticHighlighter';
@@ -64,9 +63,10 @@ import SyntaxHighlighter from '../highlighters/SyntaxHighlighter';
 import ESConverter from '../../../../shared/database/elastic/formatter/ESConverter';
 import ESJSONParser from '../../../../shared/database/elastic/parser/ESJSONParser';
 
+import './ElasticMode';
+import './TQLMode.js';
+
 // Style sheets and addons for CodeMirror
-require('./elastic.js');
-require('./tql.js');
 import 'codemirror/addon/display/placeholder.js';
 import 'codemirror/addon/edit/closebrackets.js';
 import 'codemirror/addon/edit/matchbrackets.js';
@@ -74,8 +74,6 @@ import 'codemirror/addon/fold/foldgutter.css';
 import 'codemirror/addon/lint/lint.css';
 import 'codemirror/addon/lint/lint.js';
 import 'codemirror/mode/javascript/javascript.js';
-
-import './eslint.js';
 
 import './codemirror.less';
 import './themes/cobalt.less';
