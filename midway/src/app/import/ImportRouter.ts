ce3dbfc5487c3a7a3642bc41e0592599ed69b45b
vcrfxia <victoria.f.xia281@gmail.com>
2017-08-10 15:00:35 -0700

    alphabetize.

--- midway/src/app/import/ImportRouter.ts
+++ midway/src/app/import/ImportRouter.ts
@@ -66,8 +66,8 @@ Router.post('/', async (ctx, next) =>
     return;
   }
 
-  Util.verifyParameters(authStream['fields'], ['dbid', 'dbname', 'tablename', 'filetype']);
-  Util.verifyParameters(authStream['fields'], ['originalNames', 'columnTypes', 'primaryKey', 'transformations']);
+  Util.verifyParameters(authStream['fields'], ['dbid', 'dbname', 'filetype', 'tablename']);
+  Util.verifyParameters(authStream['fields'], ['columnTypes', 'originalNames', 'primaryKey', 'transformations']);
   // optional parameters: update, hasCsvHeader
 
   ctx.body = await imprt.upsert(authStream['files'], authStream['fields'], false);
@@ -76,7 +76,7 @@ Router.post('/', async (ctx, next) =>
 Router.post('/export', passport.authenticate('access-token-local'), async (ctx, next) =>
 {
   const exprtConf: ExportConfig = ctx.request.body.body;
-  Util.verifyParameters(exprtConf, ['variantId', 'templateID']);
+  Util.verifyParameters(exprtConf, ['templateID', 'variantId']);
   ctx.body = await imprt.export(exprtConf);
 });
 
@@ -90,7 +90,7 @@ Router.post('/headless', async (ctx, next) =>
     return;
   }
 
-  Util.verifyParameters(authStream['fields'], ['templateID', 'filetype']);
+  Util.verifyParameters(authStream['fields'], ['filetype', 'templateID']);
   // optional parameters: update, hasCsvHeader
 
   ctx.body = await imprt.upsert(authStream['files'], authStream['fields'], true);
