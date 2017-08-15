1af2d3bf78c001055edbee8b2370f8c1b5401e3c
jmansor <juanpablo.mansor@gmail.com>
2017-08-15 17:00:15 -0300

    Added frontend module aliases in webpack and typescript config. Changed src/app/library/LibraryTypes roles and users modules import paths as an example.

--- src/app/library/LibraryTypes.tsx
+++ src/app/library/LibraryTypes.tsx
@@ -47,9 +47,9 @@ THE SOFTWARE.
 // tslint:disable:variable-name max-classes-per-file strict-boolean-expressions
 
 import * as Immutable from 'immutable';
+import * as RoleTypes from 'roles/RoleTypes';
 import * as _ from 'underscore';
-import * as RoleTypes from './../roles/RoleTypes';
-import * as UserTypes from './../users/UserTypes';
+import * as UserTypes from 'users/UserTypes';
 import Util from './../util/Util';
 const { List, Map } = Immutable;
 import { _ResultsConfig, ResultsConfig } from '../../../shared/results/types/ResultsConfig';
