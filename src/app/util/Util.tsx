01c60ab0021b81465142b83feb029f5c36a41108
aleiux <alexliu314@gmail.com>
2017-08-17 12:15:33 -0700

    Switched boolean expressions to use localeCompare

--- src/app/util/Util.tsx
+++ src/app/util/Util.tsx
@@ -620,9 +620,9 @@ const Util = {
     {
       if (a.type === b.type)
       {
-        return (a.name > b.name && 1) || -1;
+        return a.name.localeCompare(b.name);
       }
-      return (a.type > b.type && 1) || -1;
+      return a.type.localeCompare(b.type);
     });
   },
 
