f645ed4c6922ad04a39978328c294d2f80c7def5
aleiux <alexliu314@gmail.com>
2017-08-29 12:48:37 -0700

    Yarn fix

--- src/app/common/components/Dropdown.tsx
+++ src/app/common/components/Dropdown.tsx
@@ -275,18 +275,18 @@ class Dropdown extends TerrainComponent<Props>
     const customColor = this.colorForOption(selectedIndex);
 
     const dropdownValueStyle = [
-        this.props.canEdit ?
-          backgroundColor(
-            !this.state.open ? Colors().inputBg : customColor || Colors().active,
-            customColor || Colors().inactiveHover
-          )
-          :
-          backgroundColor(Colors().darkerHighlight)
-        ,
-        fontColor(
-          !this.state.open ? customColor || Colors().text1 : Colors().text1,
-          this.props.canEdit ? Colors().text1 : undefined,
+      this.props.canEdit ?
+        backgroundColor(
+          !this.state.open ? Colors().inputBg : customColor || Colors().active,
+          customColor || Colors().inactiveHover
         )
+        :
+        backgroundColor(Colors().darkerHighlight)
+      ,
+      fontColor(
+        !this.state.open ? customColor || Colors().text1 : Colors().text1,
+        this.props.canEdit ? Colors().text1 : undefined,
+      )
     ];
 
     return (
@@ -312,7 +312,7 @@ class Dropdown extends TerrainComponent<Props>
           ref='value'
           style={[
             { width: this.props.width },
-            ... dropdownValueStyle
+            ...dropdownValueStyle
           ]}
           key='dropdown-value'
         >
