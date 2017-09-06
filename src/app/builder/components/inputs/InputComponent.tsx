2069984f89565e4d64a62696c1811cf8dbdbe2ce
lbrouckman <laura.brouckman@gmail.com>
2017-09-06 15:46:21 -0700

    Lots broken, error handling for non-numerical inputsn up-down arrow keys, map overlays working '

--- src/app/builder/components/inputs/InputComponent.tsx
+++ src/app/builder/components/inputs/InputComponent.tsx
@@ -199,6 +199,7 @@ class InputComponent extends TerrainComponent<Props>
           location={value.location}
           markLocation={markLocation}
           showDistanceTools={true}
+          secondLocation={[40, -120]}
         />);
     }
 
