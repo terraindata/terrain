4f0fc0be0024fdb552970262c04f9f22fc02ec0f
aleiux <alexliu314@gmail.com>
2017-09-20 11:35:12 -0700

    added shared to tsconfig and package.json, added images alias

--- src/app/builder/components/cards/CardComponent.tsx
+++ src/app/builder/components/cards/CardComponent.tsx
@@ -79,9 +79,9 @@ import SchemaStore from '../../../schema/data/SchemaStore';
 import BuilderComponent from '../BuilderComponent';
 import CreateCardTool from './CreateCardTool';
 
-const ArrowIcon = require('./../../../../images/icon_arrow_8x5.svg?name=ArrowIcon');
-const HandleIcon = require('./../../../../images/icon_more_12x3.svg?name=MoreIcon');
-const HelpIcon = require('./../../../../images/icon_help-1.svg?name=HelpIcon');
+const ArrowIcon = require('images/icon_arrow_8x5.svg?name=ArrowIcon');
+const HandleIcon = require('images/icon_more_12x3.svg?name=MoreIcon');
+const HelpIcon = require('images/icon_help-1.svg?name=HelpIcon');
 
 const CARD_OVERSCAN = 200;
 const CARD_HEIGHT_MAP: { [id: string]: number } = {};
