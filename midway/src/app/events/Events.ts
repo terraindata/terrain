5e8859bfb7c4f3a19cbce0910aae608b11137953
adk9 <abd.kulkarni@gmail.com>
2017-10-06 14:45:35 -0700

    Add routes to add/remove/list events
    + Also pre-populate with predefined events

--- midway/src/app/events/Events.ts
+++ midway/src/app/events/Events.ts
@@ -81,9 +81,17 @@ export interface EventConfig
   meta?: any;
 }
 
+export interface EventMetadataConfig
+{
+  id: number | string;
+  name: string;
+  description?: string;
+}
+
 export class Events
 {
   private eventTable: Tasty.Table;
+  private eventMetadataTable: Tasty.Table;
   private elasticController: ElasticController;
 
   constructor()
@@ -103,6 +111,58 @@ export class Events
       ],
       'terrain-analytics',
     );
+
+    this.eventMetadataTable = new Tasty.Table(
+      'eventMetadata',
+      [],
+      [
+        'id',
+        'name',
+      ],
+      'terrain-analytics',
+    );
+
+    // predefined events
+    this.elasticController.getTasty().upsert(this.eventMetadataTable,
+    [
+      {
+        id: 1,
+        name: 'view',
+        description:
+          `Page view or impression event. This event fires off when a page
+           serving a variant is loaded or viewed.`,
+      },
+      {
+        id: 2,
+        name: 'click',
+        description:
+          `Item or page click event. This event is generated through an interaction
+           with the page or item served by a variant.`,
+      },
+      {
+        id: 3,
+        name: 'conversion',
+        description:
+          `An event denoting an item transaction or conversion. This event is generated
+           when an item served by a variant gets bought.`,
+      },
+      {
+        id: 4,
+        name: 'addtocart',
+        description:
+          `An event denoting that an item was added to the cart.`,
+      },
+    ]);
+  }
+
+  public async getMetadata(filter: object): Promise<string>
+  {
+    return JSON.stringify(this.elasticController.getTasty().select(this.eventMetadataTable, [], filter));
+  }
+
+  public async addEvent(event: object): Promise<EventMetadataConfig>
+  {
+    return this.elasticController.getTasty().upsert(this.eventMetadataTable, event) as Promise<EventMetadataConfig>;
   }
 
   /*
