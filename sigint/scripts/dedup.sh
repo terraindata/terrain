1074515aa7a3421b3bb642cfcdb358e42cdf2639
DABH <DABH@users.noreply.github.com>
2017-11-03 12:03:45 -0700

    Update deduplication script

--- sigint/scripts/dedup.sh
+++ sigint/scripts/dedup.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
 
-# Change these parameters...
+# Default parameters
 host="http://localhost:9200"
 index="terrain-analytics"
 type="events"
 maxHitsPerBucket=5
+verbose=0
+
+# Read parameters from command line
+while [[ "$#" > 0 ]]; do case $1 in
+    --host) host="$2"; shift; shift;;
+    --index) index="$2"; shift; shift;;
+    --type) type="$2"; shift; shift;;
+    --maxHitsPerBucket) maxHitsPerBucket=$2; shift; shift;;
+    --verbose) verbose=1; shift;;
+    *) echo "$1 is not a recognized flag"; exit 1;;
+  esac;
+done
+
+echo "Begin deleting duplicates..."
+echo "Host: $host"
+echo "Index: $index"
+echo "Type: $type"
+echo "Max hits per bucket: $maxHitsPerBucket"
+echo "Verbose: $verbose"
 
 #===================================
 
@@ -14,6 +33,8 @@ duplicateQueryResults=$(curl -s -XGET "${host}/${index}/${type}/_search" -d "
   \"query\": {
       \"match_all\": {}
   },
+  \"track_scores\": false,
+  \"sort\": \"_doc\",
   \"aggs\": {
     \"duplicateCount\": {
       \"terms\": {
@@ -23,7 +44,12 @@ duplicateQueryResults=$(curl -s -XGET "${host}/${index}/${type}/_search" -d "
       \"aggs\": {
         \"duplicateDocuments\": {
           \"top_hits\": {
-            \"size\": ${maxHitsPerBucket}
+            \"size\": ${maxHitsPerBucket},
+            \"track_scores\": false,
+            \"sort\": \"_doc\",
+            \"_source\": {
+              \"includes\": [ \"_id\" ]
+            }
           }
         }
       }
@@ -32,6 +58,15 @@ duplicateQueryResults=$(curl -s -XGET "${host}/${index}/${type}/_search" -d "
 }")
 
 numBuckets=$(echo $duplicateQueryResults | jq -r '.aggregations.duplicateCount.buckets | length')
+if [ ! "$numBuckets" ] ; then
+    echo "Error with duplicate query result..."$'\n';
+    echo $duplicateQueryResults;
+    exit 1;
+fi
+
+if [ -z $verbose ] ; then
+    echo $duplicateQueryResults;
+fi
 
 echo "Found $numBuckets buckets with duplicates..."
 
@@ -42,8 +77,8 @@ fi
 
 docsToRemove=()
 for i in $(seq 0 $((${numBuckets}-1))); do
-    dups=($(echo $r1 | jq -r ".aggregations.duplicateCount.buckets[${i}].duplicateDocuments.hits.hits[]._id")) ;
-    numDups=($(echo $r1 | jq -r ".aggregations.duplicateCount.buckets[${i}].duplicateDocuments.hits.hits | length")) ;
+    dups=($(echo $duplicateQueryResults | jq -r ".aggregations.duplicateCount.buckets[${i}].duplicateDocuments.hits.hits[]._id")) ;
+    numDups=($(echo $duplicateQueryResults | jq -r ".aggregations.duplicateCount.buckets[${i}].duplicateDocuments.hits.hits | length")) ;
     echo "In bucket $i, found $numDups duplicates..." ;
     dups=(${dups[@]:1}) # leave in the first event, just delete the duplicates of that event
     docsToRemove=("${docsToRemove[@]}" "${dups[@]}")
@@ -59,6 +94,10 @@ done
 
 echo "Executing bulk delete query..."
 
-curl -XPOST "${host}/${index}/_bulk" -H 'Content-Type: application/json' --data-binary "${deleteQuery}"
+bulkQueryResult=$(curl -XPOST "${host}/${index}/_bulk" -H 'Content-Type: application/json' --data-binary "${deleteQuery}")
 
-echo "Finished deletion"
+if [ -z $verbose ] ; then
+    echo $bulkQueryResult;
+fi
+
+echo $'\n'"Finished deletion"
