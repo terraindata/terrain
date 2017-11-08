3a3124a77638ce1fe0cb374dbabc19eca9e608c9
yangxi <hiyangxi@gmail.com>
2017-11-08 16:17:13 +1100

    Minor improvement about the SPEC. + Introduce any_value, any_object, and any_array types, so that we can always mark any value, object, array. + Add some logs and assertions.

--- src/database/elastic/conversion/ElasticToCards.tsx
+++ src/database/elastic/conversion/ElasticToCards.tsx
@@ -111,6 +111,7 @@ export default function ElasticToCards(
     }
     catch (e)
     {
+      console.error(e);
       return query
         .set('cardsAndCodeInSync', false);
     }
@@ -264,6 +265,8 @@ function isDistanceCard(valueInfo: ESValueInfo): boolean
 
 function isFilterCard(valueInfo: ESValueInfo): boolean
 {
+  console.assert(valueInfo.clause, 'ValueInfo ' + JSON.stringify(valueInfo.value) + ' does not have the clause type');
+
   const isBool = (valueInfo.clause.clauseType === ESClauseType.ESStructureClause) &&
     (valueInfo.clause.name === 'bool');
 
