6a734c22c9ee5e8fd20c6659bf28daf2863efb29
DABH <DABH@users.noreply.github.com>
2017-11-08 18:18:55 -0800

    enhance customization options for test setup/teardown scripts, and setup/teardown elastic image for sigint tests in CI pipeline

--- midway/test/scripts/setup_env.sh
+++ midway/test/scripts/setup_env.sh
@@ -11,6 +11,9 @@ sqlite_image="terrain/moviesdb-sqlite:$deploy_version"
 mysql_port=3306
 elastic_port=9200
 sqlite_path=${DIR}/../../../
+use_mysql=1
+use_elastic=1
+use_sqlite=1
 
 function usage {
 	 echo "Help!"
@@ -30,6 +33,9 @@ do
 		  --mysql-port=*)
 				mysql_port="${1#*=}"
 				;;
+		  --use-mysql=*)
+				use_mysql="${1#*=}"
+				;;
 		  --elastic-port)
 				elastic_port=$2
 				shift
@@ -37,12 +43,18 @@ do
 		  --elastic-port=*)
 				elastic_port="${1#*=}"
 				;;
+		  --use-elastic=*)
+				use_elastic="${1#*=}"
+				;;
 		  --sqlite-path)
 				sqlite_path=$2
 				shift
 				;;
 		  --sqlite-path=*)
 				sqlite_path="${1#*=}"
+				;;
+		  --use-sqlite=*)
+				use_sqlite="${1#*=}"
 				;;
 		  --version)
 				echo "Elastic: $elastic_image MySQL: $mysql_image SQLite: $sqlite_image"
@@ -61,31 +73,53 @@ do
 done
 
 # stop any existing instances
-${DIR}/teardown_env.sh
+${DIR}/teardown_env.sh --use-mysql=${use_mysql} --use-elastic=${use_elastic} --use-sqlite=${use_sqlite}
 
-if [ "$mysql_port" == 3306 ];
+if [ "$use_mysql" == 1 ];
 then
-	 echo "Starting mysql on port $mysql_port..."
-else
-	 echo "Starting mysql on NON-STANDARD port $mysql_port..."
+    if [ "$mysql_port" == 3306 ];
+    then
+        echo "Starting mysql on port $mysql_port..."
+    else
+        echo "Starting mysql on NON-STANDARD port $mysql_port..."
+    fi
 fi
 
-if [ "$elastic_port" == 9200 ];
+if [ "$use_elastic" == 1 ];
 then
-	 echo "Starting elastic on port $elastic_port..."
-else
-	 echo "Starting elastic on NON-STANDARD port $elastic_port..."
+    if [ "$elastic_port" == 9200 ];
+    then
+    	 echo "Starting elastic on port $elastic_port..."
+    else
+    	 echo "Starting elastic on NON-STANDARD port $elastic_port..."
+    fi
 fi
 
 # Pull images in parallel in order to minimize latency...
-docker pull $sqlite_image &
-docker pull $mysql_image &
-docker pull $elastic_image &
+if [ "$use_sqlite" == 1 ]; then
+    docker pull $sqlite_image &
+fi
+if [ "$use_mysql" == 1 ]; then
+    docker pull $mysql_image &
+fi
+if [ "$use_elastic" == 1 ]; then
+    docker pull $elastic_image &
+fi
 wait
 
-docker run -v${sqlite_path}:/data/ -u$(id -u):$(id -g) $sqlite_image
-MYSQL_ID=$(docker run -d --name moviesdb-mysql -p $mysql_port:$mysql_port $mysql_image)
-ELASTIC_ID=$(docker run -d --name moviesdb-elk -p $elastic_port:$elastic_port $elastic_image)
+if [ "$use_sqlite" == 1 ]; then
+    docker run -v${sqlite_path}:/data/ -u$(id -u):$(id -g) $sqlite_image
+fi
+if [ "$use_mysql" == 1 ]; then
+    MYSQL_ID=$(docker run -d --name moviesdb-mysql -p $mysql_port:$mysql_port $mysql_image)
+else
+    MYSQL_ID=1
+fi
+if [ "$use_elastic" == 1 ]; then
+    ELASTIC_ID=$(docker run -d --name moviesdb-elk -p $elastic_port:$elastic_port $elastic_image)
+else
+    ELASTIC_ID=1
+fi
 
 if [ -z "$ELASTIC_ID" -o -z "$MYSQL_ID" ]; then
 	echo "Docker services failed to start..."
@@ -94,5 +128,10 @@ fi
 
 echo "Waiting on services to be ready..."
 
-while [ "$(docker inspect -f {{.State.Health.Status}} moviesdb-mysql)" != "healthy" -o "$(docker inspect -f {{.State.Health.Status}} moviesdb-elk)" != "healthy" ]; do sleep 0.1; done;
+if [ "$use_mysql" == 1 ]; then
+    while [ "$(docker inspect -f {{.State.Health.Status}} moviesdb-mysql)" != "healthy" ]; do sleep 0.1; done;
+fi
+if [ "$use_elastic" == 1 ]; then
+    while [ "$(docker inspect -f {{.State.Health.Status}} moviesdb-elk)" != "healthy" ]; do sleep 0.1; done;
+fi
 
