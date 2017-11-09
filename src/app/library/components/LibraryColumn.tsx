becfdfbf743bcbc431d4860fc6bcce136dd94c66
adk9 <abd.kulkarni@gmail.com>
2017-11-09 15:05:45 -0800

    Silence lint error

--- src/app/library/components/LibraryColumn.tsx
+++ src/app/library/components/LibraryColumn.tsx
@@ -44,7 +44,7 @@ THE SOFTWARE.
 
 // Copyright 2017 Terrain Data, Inc.
 
-// tslint:disable:restrict-plus-operands strict-boolean-expressions
+// tslint:disable:no-var-requires restrict-plus-operands strict-boolean-expressions
 
 import './LibraryColumn.less';
 
