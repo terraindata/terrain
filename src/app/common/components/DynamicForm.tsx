e1cd030aba0d0e77aa352cf6b0cc069ad3ad538a
aleiux <alexliu314@gmail.com>
2018-02-13 21:50:58 -0800

    Cleaned and Polished DynamicForm

--- src/app/common/components/DynamicForm.tsx
+++ src/app/common/components/DynamicForm.tsx
@@ -59,14 +59,61 @@ const { List, Map } = Immutable;
 import Autocomplete from 'common/components/Autocomplete';
 import CheckBox from 'common/components/CheckBox';
 import Dropdown from 'common/components/Dropdown';
+import FadeInOut from 'common/components/FadeInOut';
+
+// DynamicForm associated Types
+export enum DisplayState
+{
+  Active,
+  Inactive,
+  Hidden,
+}
+
+export enum DisplayType
+{
+  TextBox = 'TextBox',
+  NumberBox = 'NumberBox',
+  CheckBox = 'CheckBox',
+}
+
+export interface InputDeclarationOptionTypes
+{
+  TextBox: {
+    acOptions?: List<string>;
+  };
+  NumberBox: {
+    acOptions?: List<string>;
+    integerOnly?: boolean; // not implemented yet, just an example
+  };
+  CheckBox: {
+    large?: boolean;
+  };
+}
+export type OptionType<K extends keyof InputDeclarationOptionTypes> = InputDeclarationOptionTypes[K];
+
+export interface InputDeclarationType<S>
+{
+  type: string;
+  options: any; // one of InputDeclarationOptionTypes
+  group?: string; // inputs with the same group value will show in a row
+  displayName?: string; // defaults to type
+  shouldShow?: (state: S) => DisplayState;
+}
+
+export type InputDeclarationMap<State extends {[k: string]: any}> =
+{
+  [key in keyof State]: InputDeclaration<State>; // is an inputDeclaration
+}
 
 export interface Props<FState>
 {
-  inputMap: InputDeclarationMap<FState>;
+  inputMap: InputDeclarationMap<FState>; // inputMap is memoized, so be careful about changing its properties!
   inputState: FState;
   onStateChange: (newState: FState) => void;
   handleConfirm: () => void;
 }
+
+// DynamicForm component types
 
 type MatrixType<S> = List<MatrixRowType<S>>; // list of list of functions
 type MatrixRowType<S> = List<MatrixCellFn<S>>;
@@ -74,16 +121,118 @@ type MatrixCellFn<S> = (state: S, key) => any;
 
 export default class DynamicForm<S> extends TerrainComponent<Props<S>>
 {
-  public renderMatrix: MatrixType<S>
+  public renderFnLookup:
+    {[K in DisplayType]: (inputInfo, stateName, state, index, disabled) => any} =
+  {
+    [DisplayType.CheckBox]: this.renderCheckBox,
+    [DisplayType.NumberBox]: this.renderNumberBox,
+    [DisplayType.TextBox]: this.renderTextBox,
+  }
 
   constructor(props)
   {
     super(props);
+    this.setStateHOC = _.memoize(this.setStateHOC);
+    this.computeRenderMatrix = memoizeOne(this.computeRenderMatrix);
+  }
+
+  public renderTextBox(inputInfo: InputDeclarationType<S>, stateName, state: S, index, disabled: boolean)
+  {
+    const options = inputInfo.options as OptionType<DisplayType.TextBox> || {};
+    return (
+      <div className='te-autocomplete-block' key={index}>
+        <div className='te-label' style={fontColor(Colors().text2)}> {inputInfo.displayName} </div>
+        <Autocomplete
+          className='te-autocomplete'
+          value={this.props.inputState[stateName]}
+          onChange={this.setStateHOC(stateName)}
+          options={options.acOptions}
+          disabled={disabled}
+        />
+      </div>
+    );
+  }
+
+  // TODO make this only accept numbers
+  public renderNumberBox(inputInfo: InputDeclarationType<S>, stateName, state: S, index, disabled: boolean)
+  {
+    const options = inputInfo.options as OptionType<DisplayType.TextBox> || {};
+    return (
+      <div className='te-autocomplete-block' key={index}>
+        <div className='te-label' style={fontColor(Colors().text2)}> {inputInfo.displayName} </div>
+        <Autocomplete
+          className='te-autocomplete'
+          value={this.props.inputState[stateName]}
+          onChange={this.setStateHOC(stateName)}
+          options={options.acOptions}
+          disabled={disabled}
+        />
+      </div>
+    );
+  }
+
+  public renderCheckBox(inputInfo: InputDeclarationType<S>, stateName, state: S, index, disabled: boolean)
+  {
+    const options = inputInfo.options as OptionType<DisplayType.CheckBox> || {};
+    return (
+     <div
+        className='te-checkbox-row'
+        key={index}
+        onClick={noop(disabled,
+            this.setStateWithTransformHOC(stateName, (value, inputState) => !inputState[stateName] )
+          )}
+      >
+        <CheckBox
+          className='te-checkbox'
+          disabled={disabled}
+          checked={this.props.inputState[stateName]}
+          onChange={() => null}
+          large={options.large}
+        />
+        <div className='te-label'> {inputInfo.displayName} </div>
+      </div>
+    );
+  }
+
+  public renderInputElement(inputInfo: InputDeclarationType<S>, stateName, state: S, index): any
+  {
+    const displayState = inputInfo.shouldShow(state);
+    const renderFn = this.renderFnLookup[inputInfo.type];
+    return (
+      <FadeInOut
+        open={displayState === DisplayState.Hidden}
+      >
+        { renderFn(inputInfo, stateName, state, index, displayState) }
+      </FadeInOut>
+    );
+  }
+
+  public computeRenderMatrix(inputMap: InputDeclarationMap<S>)
+  {
+    let renderMatrix: MatrixType<S> = List([]);
+    const groupToIndex = {};
+    for (const stateName of Object.keys(inputMap))
+    {
+      const { group } = inputMap[stateName];
+      const inputInfo: InputDeclarationType<S> = _.defaults({}, inputMap[stateName],
+        { displayName: stateName, shouldShow: () => true }
+      );
+      let useIndex = renderMatrix.size;
+      if (group !== undefined)
+      {
+        useIndex = _.defaults(groupToIndex, { group: useIndex })[group];
+      }
+      renderMatrix = renderMatrix.updateIn([useIndex], List([]), (value) =>
+      {
+        return (state, index) => this.renderInputElement(inputInfo, stateName, state, index);
+      });
+    }
+    return renderMatrix;
   }
 
   public renderMatrixCell(cellFn: MatrixCellFn<S>, index)
   {
-
+    return cellFn(this.props.inputState, index);
   }
 
   public renderMatrixRow(row: MatrixRowType<S>, index)
@@ -99,82 +248,61 @@ export default class DynamicForm<S> extends TerrainComponent<Props<S>>
 
   public render()
   {
+    const renderMatrix: MatrixType<S> = this.computeRenderMatrix(this.props.inputMap);
     return (
       <div>
-        {
-          this.renderMatrix.map(this.renderMatrixRow)
-        }
+        { renderMatrix.map(this.renderMatrixRow) }
       </div>
     );
   }
+
+  // optional transformValue can change the value based on the state. This function is not transformed
+  public setStateWithTransformHOC(stateName, transformValue: (value, state?: S) => any = (value) => value)
+  {
+    return (value) => {
+      const shallowCopy = _.clone(this.props.inputState);
+      const newValue = transformValue(value, this.props.inputState);
+      shallowCopy[stateName] = newValue;
+      this.props.onStateChange(shallowCopy);
+    }
+  }
+
+  // This function is memoized
+  public setStateHOC(stateName)
+  {
+    return (value) => {
+      const shallowCopy = _.clone(this.props.inputState);
+      shallowCopy[stateName] = value;
+      this.props.onStateChange(shallowCopy);
+    }
+  }
 }
 
-export enum DisplayState
+function noop(disabled: boolean, fn)
 {
-  Active,
-  Inactive,
-  Hidden,
+  return disabled ? () => null : fn;
 }
 
-export enum DisplayType
-{
-  TextBox = 'TextBox',
-  NumberBox = 'NumberBox',
-  CheckBox = 'CheckBox',
-  NumberRange = 'NumberRange',
-}
-
-// important
-export interface InputDeclarationOptionTypes
-{
-  TextBox: {
-    __acceptedTypes?: string;
-    randomThing: string;
-  };
-  NumberBox: {
-    __acceptedTypes?: number;
-  };
-  NumberRange: {
-    __acceptedTypes?: number;
-    from: number;
-    to: number;
-  };
-  CheckBox: {
-    __acceptedTypes?: boolean;
-  };
-}
-
-// important
-export interface InputDeclarationType<S = any>
-{
-  type: string;
-  options: any; // one of InputDeclarationOptionTypes
-  group?: string; // inputs with the same group value will show in a row
-  displayName?: string; // defaults to type
-  shouldShow?: (state: S) => DisplayState;
-}
-
-/*** TYPE SORCERY! ***/
+/*** Dynamic Form TYPE SORCERY! ***/
 type DisplayTypeKeys = keyof typeof DisplayType;
 
+// make sure that the DisplayType enum has keys equal to its values
 type AssertEnumValuesEqualKeys = {
   [K in DisplayTypeKeys]: K
 };
-DisplayType as AssertEnumValuesEqualKeys;
+DisplayType as AssertEnumValuesEqualKeys; // if this is giving errors, double check that
+// DisplayTypeKeys are equal to its values
 
+// make sure that for every item in the DisplayType enum there's an associated InputDeclaration
 type AssertOptionTypesExhaustive = {
   [K in DisplayType]: InputDeclarationOptionTypes[K]
 }
 
+// below are types that do some magic to add type checking to InputDeclarationMap
 interface InputDeclarationHelper<K extends DisplayTypeKeys, State> extends InputDeclarationType<State>
 {
   type: K;
   options: InputDeclarationOptionTypes[K];
-}
-
-type InputDeclarationMap<State extends {[k: string]: any}> =
-{
-  [key in keyof State]: InputDeclaration<State>;
 }
 
 type InputDeclarationBundle<State> = {
@@ -182,42 +310,40 @@ type InputDeclarationBundle<State> = {
 }
 
 type InputDeclaration<State> = InputDeclarationBundle<State>[keyof InputDeclarationBundle<State>];
+/*** End of Type Sorcery ***/
 
-type InputDeclarationOptionType<K extends keyof InputDeclarationOptionTypes> = InputDeclarationOptionTypes[K];
-/*** Type Sorcery ***/
+// interface FormState
+// {
+//   from: number;
+//   length: number;
+//   textField: string;
+//   flag: boolean;
+// }
 
-interface FormState
-{
-  from: number;
-  length: number;
-  textField: string;
-  flag: boolean;
-}
+// // should succeed
+// const DeclarationMap: InputDeclarationMap<FormState> =
+// {
+//   from: {
+//     type: DisplayType.NumberBox,
+//     options: {
 
-// should succeed
-const DeclarationMap: InputDeclarationMap<FormState> =
-{
-  from: {
-    type: DisplayType.NumberBox,
-    options: {
-
-    }
-  },
-  length: {
-    type: DisplayType.NumberRange,
-    options: {
-      from: 0,
-      to: -1,
-    }
-  },
-  textField: {
-    type: DisplayType.TextBox,
-    options: {
-      randomThing: 'test',
-    }
-  },
-  flag: {
-    type: DisplayType.CheckBox,
-    options: {}
-  }
-}
+//     }
+//   },
+//   length: {
+//     type: DisplayType.NumberRange,
+//     options: {
+//       from: 0,
+//       to: -1,
+//     }
+//   },
+//   textField: {
+//     type: DisplayType.TextBox,
+//     options: {
+//       randomThing: 'test',
+//     }
+//   },
+//   flag: {
+//     type: DisplayType.CheckBox,
+//     options: {}
+//   }
+// }
