2feba536412249722b9d35e5d485c8d1353be6c5
jmansor <juanpablo.mansor@gmail.com>
2018-03-12 17:44:24 -0300

    Added postgres_changes.sh to hold small schema changes that don't require dropping the whole table, or for tables that are completely replaced.

---
+++ midway/migrations/postgres_changes.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PG_DB_NAME="midway"
+PG_USER_NAME="t3rr41n-demo"
+PG_PASSWORD="r3curs1v3$"
+
+updateTable_statusHistory="UPDATE items\
+  SET status='LOCK'\
+  WHERE status='LIVE'"
+
+PGPASSWORD=$PG_PASSWORD psql -U $PG_USER_NAME -d $PG_DB_NAME -h localhost -p 5432 -c "$updateTable_statusHistory"
