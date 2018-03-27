12c9573e25cb26e9c77d483ae86eaa73d7080bb5
adk9 <abd.kulkarni@gmail.com>
2018-03-27 16:43:06 -0700

    fix typo in ElasticReader [skip ci]

--- midway/src/database/elastic/streams/ElasticReader.ts
+++ midway/src/database/elastic/streams/ElasticReader.ts
@@ -92,8 +92,8 @@ export class ElasticReader extends Stream.Readable
 
       if (this.streaming)
       {
-        body['scroll'] = this.scroll,
-          body['size'] = Math.min(this.size, this.MAX_SEARCH_SIZE);
+        body['scroll'] = this.scroll;
+        body['size'] = Math.min(this.size, this.MAX_SEARCH_SIZE);
       }
 
       this.querying = true;
