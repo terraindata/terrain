93b42e673474b0c7c2ea3382312fb4ed3430f2d1
adk9 <abd.kulkarni@gmail.com>
2018-04-04 13:18:43 -0700

    Always set size, whether streaming or not

--- midway/src/database/elastic/streams/ElasticReader.ts
+++ midway/src/database/elastic/streams/ElasticReader.ts
@@ -93,8 +93,9 @@ export class ElasticReader extends SafeReadable
       if (this.streaming)
       {
         body['scroll'] = this.scroll;
-        body['size'] = Math.min(this.size, this.MAX_SEARCH_SIZE);
       }
+
+      body['size'] = Math.min(this.size, this.MAX_SEARCH_SIZE);
 
       this.querying = true;
       this.client.search(
