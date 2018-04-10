1a2c8e55e7bf00837804402b7c42d0b980b744e9
adk9 <abd.kulkarni@gmail.com>
2018-04-09 18:01:05 -0700

    Add back support to export results as a JSON object

--- src/app/fileImport/components/FileImportPreview.tsx
+++ src/app/fileImport/components/FileImportPreview.tsx
@@ -740,12 +740,15 @@ class FileImportPreview extends TerrainComponent<Props>
         stringQuery =
           ESParseTreeToCode(this.props.query.parseTree.parser as ESJSONParser, { replaceInputs: true }, this.props.inputs);
       }
+
+      const fileExtension = this.props.filetype.substr(0, this.props.filetype.indexOf(' '));
+
       Actions.exportFile(
         stringQuery,
         this.props.serverId,
         this.props.exportRank,
         this.state.typeObjectKey,
-        this.props.algorithmName + '_' + String(moment().format('MM-DD-YY')) + '.' + this.props.filetype,
+        this.props.algorithmName + '_' + String(moment().format('MM-DD-YY')) + '.' + fileExtension,
         this.handleFileExportSuccess,
         this.handleFileExportError,
       );
