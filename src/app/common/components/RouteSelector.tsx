ec678dcecb783193cfe7c06bf102e14197ec6602
lbrouckman <laura.brouckman@gmail.com>
2018-04-12 12:11:52 -0700

    yarn

--- src/app/common/components/RouteSelector.tsx
+++ src/app/common/components/RouteSelector.tsx
@@ -805,13 +805,13 @@ export class RouteSelector extends TerrainComponent<Props>
                       </div>,
                       option.tooltip,
                     )}
-                      <FadeInOut
-                        open={isSelected}
-                      >
-                      {
-                        option.component
-                      }
-                      </FadeInOut>
+                  <FadeInOut
+                    open={isSelected}
+                  >
+                    {
+                      option.component
+                    }
+                  </FadeInOut>
                 </div>
               }
               {
