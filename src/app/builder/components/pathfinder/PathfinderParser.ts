e27927410f3f67deeeeeded7363728b4a886dc0f
lbrouckman <laura.brouckman@gmail.com>
2018-04-24 11:31:43 -0700

    yarn

--- src/app/builder/components/pathfinder/PathfinderParser.ts
+++ src/app/builder/components/pathfinder/PathfinderParser.ts
@@ -353,7 +353,7 @@ function parseFilters(filterGroup: FilterGroup, inputs, inMatchQualityContext = 
   });
   let must = List([]);
   let mustNot = List([]);
-  let filter = List([]);
+  const filter = List([]);
   let should = List([]);
   let useShould = false;
   if (filterGroup.minMatches !== 'all' || inMatchQualityContext)
