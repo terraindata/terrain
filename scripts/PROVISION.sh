eb51ac8c1fcc981fe6470a8453bfc2a10144e3e5
jason4012 <1808154+jason4012@users.noreply.github.com>
2018-04-24 16:06:41 -0700

    Update provisioning script to include dropping old tables that will conflict with the new ETL-scheduler system

--- scripts/PROVISION.sh
+++ scripts/PROVISION.sh
@@ -4,3 +4,8 @@
 defaultUserAccessToken=`curl -s -XPOST -H 'Content-Type: application/json' -d '{"email":"admin@terraindata.com","password":"CnAATPys6tEB*ypTvqRRP5@2fUzTuY!C^LZP#tBQcJiC*5"}' 'localhost:3000/midway/v1/auth/login' | jq .accessToken | sed -e 's/"//g'`
 echo "${defaultUserAccessToken}"
 
+## drop pre-ETL scheduler and ETL-related tables
+PGPASSWORD="r3curs1v3$" psql -U "t3rr41n-demo" -d midway -h localhost -c 'DROP TABLE IF EXISTS schedules; ' -p 5432
+PGPASSWORD="r3curs1v3$" psql -U "t3rr41n-demo" -d midway -h localhost -c 'DROP TABLE IF EXISTS schedulerLogs; ' -p 5432
+PGPASSWORD="r3curs1v3$" psql -U "t3rr41n-demo" -d midway -h localhost -c 'DROP TABLE IF EXISTS jobs; ' -p 5432
+
