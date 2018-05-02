23ec84389cc706fa2c1c9599ac3eeac0fe244016
jmansor <juanpablo.mansor@gmail.com>
2018-05-02 15:43:59 -0300

    Added SimpleTable component.

---
+++ src/app/common/components/SimpleTable.tsx
@@ -0,0 +1,113 @@
+/*
+University of Illinois/NCSA Open Source License 
+
+Copyright (c) 2018 Terrain Data, Inc. and the authors. All rights reserved.
+
+Developed by: Terrain Data, Inc. and
+              the individuals who committed the code in this file.
+              https://github.com/terraindata/terrain
+                  
+Permission is hereby granted, free of charge, to any person 
+obtaining a copy of this software and associated documentation files 
+(the "Software"), to deal with the Software without restriction, 
+including without limitation the rights to use, copy, modify, merge,
+publish, distribute, sublicense, and/or sell copies of the Software, 
+and to permit persons to whom the Software is furnished to do so, 
+subject to the following conditions:
+
+* Redistributions of source code must retain the above copyright notice, 
+  this list of conditions and the following disclaimers.
+
+* Redistributions in binary form must reproduce the above copyright 
+  notice, this list of conditions and the following disclaimers in the 
+  documentation and/or other materials provided with the distribution.
+
+* Neither the names of Terrain Data, Inc., Terrain, nor the names of its 
+  contributors may be used to endorse or promote products derived from
+  this Software without specific prior written permission.
+
+This license supersedes any copyright notice, license, or related statement
+following this comment block.  All files in this repository are provided
+under the same license, regardless of whether a corresponding comment block
+appears in them.  This license also applies retroactively to any previous
+state of the repository, including different branches and commits, which
+were made public on or after December 8th, 2018.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS 
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS WITH
+THE SOFTWARE.
+*/
+
+// Copyright 2017 Terrain Data, Inc.
+
+// tslint:disable:no-var-requires strict-boolean-expressions
+
+import { List } from 'immutable';
+import * as React from 'react';
+import * as Immutable from 'immutable';
+
+import './Table.less';
+
+import TerrainComponent from './TerrainComponent';
+
+export interface SimpleTableColumn
+{
+  columnKey: string;
+  columnLabel: string;
+}
+
+export interface Props
+{
+  header: SimpleTableColumn[];
+  data: Immutable.Map<ID, any>;
+}
+
+export class SimpleTable extends TerrainComponent<Props>
+{
+  public state: {};
+
+  public render()
+  {
+    const { header, data } = this.props;
+
+    const columnKeys = header.reduce((keys, column) => keys.concat(column.columnKey), []);
+
+    return (
+      <table className="simple-table">
+        <thead className="simple-table-header">
+          <tr className="simple-table-row">
+          {
+            header.map((column, index) =>
+            {
+              return <th key={column.columnKey} className="simple-table-cell">{column.columnLabel}</th>
+            })
+          }
+          </tr>
+        </thead>
+        <tbody className="simple-table-body">
+          {
+            data.valueSeq().map(entry =>
+            {
+              return (
+                <tr key={entry.id} className="simple-table-row">
+                  {
+                    columnKeys.map((key, index) =>
+                    {
+                      return <td key={key} className="simple-table-cell">{entry[key]}</td>
+                    })
+                  }
+                </tr>
+              )
+            })
+          }
+        </tbody>
+      </table>
+    );
+  }
+}
+
+export default SimpleTable;
