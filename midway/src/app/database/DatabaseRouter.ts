36d572c6c64d06ef0688066e5e0a4dacb8997eb5
adk9 <abd.kulkarni@gmail.com>
2018-05-03 16:05:51 -0700

    Remove status [skip ci]

--- midway/src/app/database/DatabaseRouter.ts
+++ midway/src/app/database/DatabaseRouter.ts
@@ -58,13 +58,13 @@ export const databases = new Databases();
 Router.get('/', passport.authenticate('access-token-local'), async (ctx, next) =>
 {
   winston.info('getting all databases');
-  ctx.body = await databases.select(['id', 'name', 'type', 'host', 'status', 'isAnalytics', 'analyticsIndex', 'analyticsType']);
+  ctx.body = await databases.select(['id', 'name', 'type', 'host', 'isAnalytics', 'analyticsIndex', 'analyticsType']);
 });
 
 Router.get('/:id', passport.authenticate('access-token-local'), async (ctx, next) =>
 {
   winston.info('getting database ID ' + String(ctx.params.id));
-  ctx.body = await databases.get(Number(ctx.params.id), ['id', 'name', 'type', 'host', 'status']);
+  ctx.body = await databases.get(Number(ctx.params.id), ['id', 'name', 'type', 'host']);
 });
 
 Router.post('/', passport.authenticate('access-token-local'), async (ctx, next) =>
@@ -85,7 +85,6 @@ Router.post('/', passport.authenticate('access-token-local'), async (ctx, next) 
     }
   }
 
-  db.status = 'DISCONNECTED';
   ctx.body = await databases.upsert(ctx.state.user, db);
   const id = Number(ctx.body[0].id);
   await databases.connect(ctx.state.user, id);
