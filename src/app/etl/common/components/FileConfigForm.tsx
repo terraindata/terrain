1299eb52d4b8b4eeadd1252a99e41b1ad9211d53
lbrouckman <laura.brouckman@gmail.com>
2018-05-10 12:07:06 -0700

    Merged with milestone branch

--- src/app/etl/common/components/FileConfigForm.tsx
+++ src/app/etl/common/components/FileConfigForm.tsx
@@ -58,6 +58,7 @@ import { instanceFnDecorator } from 'shared/util/Classes';
 import { _FileConfig, _SourceConfig, FileConfig, SinkConfig, SourceConfig } from 'shared/etl/immutable/EndpointRecords';
 import { FileConfig as FileConfigI } from 'shared/etl/types/EndpointTypes';
 import { FileTypes, Languages } from 'shared/etl/types/ETLTypes';
+import Util from 'app/util/Util';
 
 const { List } = Immutable;
 
@@ -127,7 +128,7 @@ export default class FileConfigForm extends TerrainComponent<Props>
   @instanceFnDecorator(memoizeOne)
   public configToState(config: FileConfig): FileConfigI
   {
-    return config.toJS() as FileConfigI;
+    return Util.asJS(config) as FileConfigI;
   }
 
   public handleFormChange(state: FileConfigI, isBlur?: boolean)
