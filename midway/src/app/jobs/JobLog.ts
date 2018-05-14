6bfae370106f4d20aa05babb15c5ecf600f7fcb7
jason4012 <1808154+jason4012@users.noreply.github.com>
2018-05-14 15:54:37 -0700

    Add job log table + JobLog support to midway + add startTime and endTime to jobs

--- midway/src/app/jobs/JobLog.ts
+++ midway/src/app/jobs/JobLog.ts
@@ -42,27 +42,80 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS WITH
 THE SOFTWARE.
 */
 
-// Copyright 2017 Terrain Data, Inc.
+// Copyright 2018 Terrain Data, Inc.
 
-export class JobConfig
+import * as _ from 'lodash';
+import * as stream from 'stream';
+import * as winston from 'winston';
+
+import * as Tasty from '../../tasty/Tasty';
+import * as App from '../App';
+import JobLogConfig from './JobLogConfig';
+
+export class JobLog
 {
-  public createdAt: Date = null;                   // when the job was created
-  public createdBy: number = null;                 // who created the job (user ID)
-  public endTime?: Date = null;                    // when the job ended
-  public id?: number = null;                       // job ID
-  public logId?: number = null;                    // jobLog table ID
-  public meta: string = '';                        // meta
-  public name: string = '';                        // name of the job
-  public pausedFilename: string = '';              // filename of the stored paused output
-  public priority: number = 1;                     // priority of the scheduled job
-  public running: boolean = false;                 // whether the task is running or not (TODO: lock this)
-  public runNowPriority: number = null;            // determines the order of jobs set to run manually. 1 is lowest priority
-  public scheduleId: number = null;                // (optional) schedule ID
-  public startTime?: Date = null;                  // when the job was started
-  public status: string = '';                      // the status of the job: success, failure, cancelled
-  public tasks: string = '';                       // a stringified representation of TaskConfig[]
-  public type: string = '';                        // whether the job is a scheduled run, a scheduled ad hoc run, or an ETL run
-  public workerId: number = null;                  // for clustering, denotes the node ID
+  private jobLogTable: Tasty.Table;
+
+  constructor()
+  {
+    this.jobLogTable = new Tasty.Table(
+      'jobLogs',
+      ['id'],
+      [
+        'contents',
+        'createdAt',
+      ],
+    );
+  }
+
+  /*
+   * PARAMS: jobId, logStream (number, stream.Readable ==> number)
+   *
+   */
+  public async create(jobId: number, logStream: stream.Readable): Promise<JobLogConfig[]>
+  {
+    return new Promise<JobLogConfig[]>(async (resolve, reject) =>
+    {
+      const jobLogs: JobLogConfig[] = await this.get(jobId);
+      if (jobLogs.length !== 0)
+      {
+        throw new Error('Job log already exists');
+      }
+
+      const newJobLog: JobLogConfig =
+      {
+        contents: '',
+        createdAt: new Date(),
+        id: jobId,
+      };
+
+      const upsertedJobLogs: JobLogConfig[] = await App.DB.upsert(this.jobLogTable, newJobLog) as JobLogConfig[];
+      resolve(upsertedJobLogs);
+
+      const updatedContentJobLog: JobLogConfig = upsertedJobLogs[0];
+      // TODO process log stream here
+      let accumulatedLog: string = ''; // set this
+      updatedContentJobLog.contents = accumulatedLog;
+      await App.DB.upsert(this.jobLogTable, updatedContentJobLog);
+    });
+  }
+
+  public async get(id?: number): Promise<JobLogConfig[]>
+  {
+    return this._select([], { id });
+  }
+
+  private async _select(columns: string[], filter: object): Promise<JobLogConfig[]>
+  {
+    return new Promise<JobLogConfig[]>(async (resolve, reject) =>
+    {
+      let rawResults: object[] = [];
+      rawResults = await App.DB.select(this.jobLogTable, columns, filter);
+
+      const results: JobLogConfig[] = rawResults.map((result: object) => new JobLogConfig(result as JobLogConfig));
+      resolve(results);
+    });
+  }
 }
 
-export default JobConfig;
+export default JobLog;
