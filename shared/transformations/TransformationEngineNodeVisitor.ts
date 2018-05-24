a276d70f75a93439003469b938946c334bd678fa
DABH <DABH@users.noreply.github.com>
2018-05-23 22:12:22 -0700

    clean up visitArrayCountNode

--- shared/transformations/TransformationEngineNodeVisitor.ts
+++ shared/transformations/TransformationEngineNodeVisitor.ts
@@ -1008,21 +1008,7 @@ export default class TransformationEngineNodeVisitor extends TransformationNodeV
       const el = yadeep.get(doc, field);
       if (Array.isArray(el))
       {
-        let count: number = 0;
-        for (let i: number = 0; i < el.length; i++)
-        {
-          let kpi: KeyPath = field;
-          if (kpi.contains('*'))
-          {
-            kpi = kpi.set(kpi.indexOf('*'), i.toString());
-          }
-          else
-          {
-            kpi = kpi.push(i.toString());
-          }
-          count++;
-        }
-        yadeep.set(doc, opts.newFieldKeyPaths.get(0), count, { create: true });
+        yadeep.set(doc, opts.newFieldKeyPaths.get(0), el.length, { create: true });
       }
       else
       {
