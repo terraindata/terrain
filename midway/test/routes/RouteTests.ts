9ec0f9f09561b3876bcf38b47378f3928a4bfed4
tombousso <tombousso@gmail.com>
2018-05-30 10:34:36 -0700

    Fix

--- midway/test/routes/RouteTests.ts
+++ midway/test/routes/RouteTests.ts
@@ -208,10 +208,6 @@ beforeAll(async (done) =>
         timezone: 'UTC',
       },
     })
-    .then((response) =>
-    {
-      done();
-    })
     .catch((error) =>
     {
       winston.warn('Error while creating test user: ' + String(error));
@@ -225,9 +221,10 @@ beforeAll(async (done) =>
   {
     // do nothing
   }
+  done();
 });
 
-afterAll(async (done) =>
+afterAll(async () =>
 {
   await DB.getDB().execute(['DROP TABLE IF EXISTS jobs;', 'DROP TABLE IF EXISTS schedules;']);
 });
