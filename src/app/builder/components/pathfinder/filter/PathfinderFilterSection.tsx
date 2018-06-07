b3e166b2198b1e53680f0443278cbc1c40b4ff9f
lbrouckman <laura.brouckman@gmail.com>
2018-06-07 15:48:44 -0700

    Fix to make inputs work in pathfinder

--- src/app/builder/components/pathfinder/filter/PathfinderFilterSection.tsx
+++ src/app/builder/components/pathfinder/filter/PathfinderFilterSection.tsx
@@ -142,12 +142,14 @@ class PathfinderFilterSection extends TerrainComponent<Props>
     // If inputs changes, or parent query data source changes, update value possibilities
     if (nextProps.pathfinderContext.builderState.query &&
       this.props.pathfinderContext.builderState.query &&
-      nextProps.pathfinderContext.builderState.query.inputs !==
-      this.props.pathfinderContext.builderState.query.inputs ||
-      !_.isEqual(nextProps.pathfinderContext.parentSource,
-        this.props.pathfinderContext.parentSource) ||
-      nextProps.pathfinderContext.parentName !==
-      this.props.pathfinderContext.parentName
+      (
+        nextProps.pathfinderContext.builderState.query.inputs !==
+        this.props.pathfinderContext.builderState.query.inputs ||
+        !_.isEqual(nextProps.pathfinderContext.parentSource,
+          this.props.pathfinderContext.parentSource) ||
+        nextProps.pathfinderContext.parentName !==
+        this.props.pathfinderContext.parentName
+      )
     )
     {
       this.setState({
