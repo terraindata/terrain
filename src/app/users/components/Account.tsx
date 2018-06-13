d6e711ab824b8231ea08ed2038820a417eda277f
adk9 <abd.kulkarni@gmail.com>
2018-06-13 11:58:52 -0700

    Add connection editor page to route switch
    [skip ci]

--- src/app/users/components/Account.tsx
+++ src/app/users/components/Account.tsx
@@ -54,6 +54,7 @@ import TerrainTabs from 'common/components/TerrainTabs';
 import TerrainComponent from '../../common/components/TerrainComponent';
 import './Account.less';
 
+import ConnectionEditorPage from './ConnectionEditorPage';
 import Connections from './Connections';
 import EditProfile from './EditProfile';
 import Notifications from './Notifications';
@@ -105,6 +106,7 @@ class Account extends TerrainComponent<Props>
                 <Route exact path='/account/settings' component={Settings} />
                 <Route exact path='/account/notifications' component={Notifications} />
                 <Route exact path='/account/connections' component={Connections} />
+                <Route exact path='/account/connections/edit/connectionId=:connectionId' component={ConnectionEditorPage} />
                 <Route exact path='/account/team' component={Team} />
               </Switch>
             </div>
