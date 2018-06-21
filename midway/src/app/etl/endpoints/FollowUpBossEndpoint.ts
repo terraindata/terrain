74238a4baa49b28a3620fbdaa2c16e4e468d16d2
DABH <DABH@users.noreply.github.com>
2018-06-21 16:59:19 -0700

    fix

--- midway/src/app/etl/endpoints/FollowUpBossEndpoint.ts
+++ midway/src/app/etl/endpoints/FollowUpBossEndpoint.ts
@@ -94,11 +94,6 @@ class FollowUpBossStream extends stream.Writable
     winston.debug('CHUNK');
     winston.debug(JSON.stringify(chunk));
 
-    if (chunk['tags'].indexOf('terrain') === -1)
-    {
-      chunk['tags'].push('terrain');
-    }
-
     if (!isNaN(chunk['FollowUpBossId']))
     {
       // Has valid FollowUpBossId, so do PUT (update)
@@ -137,10 +132,21 @@ class FollowUpBossStream extends stream.Writable
     } else
     {
       // No existing ID, so do POST (create)
+
+      const formattedChunk = chunk;
+      if (!formattedChunk['tags'])
+      {
+        formattedChunk['tags'] = ['terrain'];
+      }
+      else if (formattedChunk['tags'] && formattedChunk['tags'].indexOf('terrain') === -1)
+      {
+        formattedChunk['tags'].push('terrain');
+      }
+
       const r = {
         url: 'https://api.followupboss.com/v1/people',
         method: 'POST',
-        json: chunk,
+        json: formattedChunk,
         qs: {
           deduplicate: true,
         },
