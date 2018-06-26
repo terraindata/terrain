49ccb6bff822a2ee362b1a8878d52adf9c6a34b2
adk9 <abd.kulkarni@gmail.com>
2018-06-26 10:59:28 -0700

    Do not show Logs tab to non-admins

--- src/app/users/components/Account.tsx
+++ src/app/users/components/Account.tsx
@@ -50,6 +50,7 @@ import * as classNames from 'classnames';
 import * as React from 'react';
 import { Link, Route, Switch } from 'react-router-dom';
 
+import TerrainTools from 'app/util/TerrainTools';
 import TerrainTabs from 'common/components/TerrainTabs';
 import TerrainComponent from '../../common/components/TerrainComponent';
 import './Account.less';
@@ -77,7 +78,7 @@ class Account extends TerrainComponent<Props>
     // { key: 'profile', label: 'Profile' },
     { key: 'connections', label: 'Connections' },
     { key: 'team', label: 'Team' },
-    { key: 'logs', label: 'Logs' },
+    ...(TerrainTools.isAdmin() ? [{ key: 'logs', label: 'Logs' }] : []),
   ];
 
   public tabToRouteMap = {
@@ -85,7 +86,7 @@ class Account extends TerrainComponent<Props>
     // profile: '/account/profile',
     connections: '/account/connections',
     team: '/account/team',
-    logs: '/account/logs',
+    ...(TerrainTools.isAdmin() ? { logs: '/account/logs' } : {}),
   };
 
   public render()
@@ -111,7 +112,7 @@ class Account extends TerrainComponent<Props>
                 <Route exact path='/account/connections/edit' component={ConnectionEditorPage} />
                 <Route exact path='/account/connections/edit/connectionId=:connectionId' component={ConnectionEditorPage} />
                 <Route exact path='/account/team' component={Team} />
-                <Route exact path='/account/logs' component={Logs} />
+                { TerrainTools.isAdmin() && <Route exact path='/account/logs' component={Logs} /> }
               </Switch>
             </div>
           </TerrainTabs>
