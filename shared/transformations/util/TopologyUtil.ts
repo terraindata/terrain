2f9e1de7618c0bfc16c76ab2bf15d54ff22f2045
aleiux <alexliu314@gmail.com>
2018-06-28 22:26:20 -0700

    add topology util

--- shared/transformations/util/TopologyUtil.ts
+++ shared/transformations/util/TopologyUtil.ts
@@ -49,9 +49,100 @@ import { KeyPath } from 'shared/util/KeyPath';
 import * as yadeep from 'shared/util/yadeep';
 import EngineUtil from 'shared/transformations/util/EngineUtil';
 
+export type Relation = 'one' | 'many';
+
+/*
+General method for seeing if 2 keypaths are "compatible"
+Take both keypaths
+Match the fronts to find the longest match
+e.g.
+[foo, bar, -1, X, Y, Z]
+[foo, bar, -1, A, B, C]
+would have [foo, bar, -1] as the longest common match.
+
+Now take the remaining part of the keypath for each path.
+In this case it would be [X, Y, Z] and [A, B, C]
+As long as at least one of them is singular (no wildcards), then the two are compatible.
+
+General rule: Compatible keypaths are anything that can be posed as a one to one, many to one, or a one to many.
+*/
+
 export default class TopologyUtil
 {
-  
+  public static isKeyPathSingular(kp: KeyPath, from = 0): boolean
+  {
+    for (let i = from; i < kp.size; i++)
+    {
+      if (kp.get(i) === -1)
+      {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  // returns the first position where the keypaths differ
+  public static getDifferingBaseIndex(kp1: KeyPath, kp2: KeyPath): number
+  {
+    let i;
+    const minSize = Math.min(kp1.size, kp2.size);
+    for (i = 0; i < minSize; i++)
+    {
+      if (kp1.get(i) !== kp2.get(i))
+      {
+        break;
+      }
+    }
+    return i;
+  }
+
+  /*
+   *  do the keypaths represent one-to-one, one-to-many, many-to-one, or many-to-many relationships?
+   */
+  public static getRelation(kp1: KeyPath, kp2: KeyPath): [Relation, Relation]
+  {
+    const baseIndex = TopologyUtil.getDifferingBaseIndex(kp1, kp2);
+    const relation1 = TopologyUtil.isKeyPathSingular(kp1, baseIndex) ? 'one' : 'many';
+    const relation2 = TopologyUtil.isKeyPathSingular(kp2, baseIndex) ? 'one' : 'many';
+    return [relation1, relation2];
+  }
+
+  /*
+   *  Given two keypaths that are one-to-one related, return a function that
+   *  translates singular paths that match kp1 to singular paths that match kp2.
+   *  E.G. kp1 is [foo, -1, bar] and kp2 is [foo, -1, baz]
+   *  createOneToOneMatcher(kp1, kp2)([foo, 3, bar]) ---> [foo, 3, baz]
+   */
+  public static createOneToOneMatcher(kp1: KeyPath, kp2: KeyPath): (kp: KeyPath) => KeyPath
+  {
+    const baseIndex = TopologyUtil.getDifferingBaseIndex(kp1, kp2);
+    const wildcards = [];
+    for (let i = 0; i < baseIndex; i++)
+    {
+      if (kp1.get(i) === -1)
+      {
+        wildcards.push(i);
+      }
+    }
+    if (wildcards.length === 0)
+    {
+      return (kp) => kp;
+    }
+    else
+    {
+      // in the absence of benchmarking to find out what's most efficient
+      // assume most times we only do 1 or 2 sets
+      return (kp: KeyPath) => {
+        let ret = kp2;
+        for (const i of wildcards)
+        {
+          ret = kp2.set(i, kp.get(i));
+        }
+        return ret;
+      };
+    }
+  }
+
   /*
    * referenceKP can contain -1
    * matchKP should not contain -1
@@ -111,9 +202,9 @@ export default class TopologyUtil
   // [a] would not be local to [c, -1, d]
   public static areFieldsLocal(kp1, kp2): boolean
   {
-    const lastIndex1: number = kp1.findLastIndex((value, index) => !EngineUtil.isNamedField(kp1, index));
+    const lastIndex1: number = kp1.findLastIndex((value, index) => EngineUtil.isWildcardField(kp1, index));
     const concretePath1 = kp1.slice(0, lastIndex1 + 1);
-    const lastIndex2: number = kp2.findLastIndex((value, index) => !EngineUtil.isNamedField(kp2, index));
+    const lastIndex2: number = kp2.findLastIndex((value, index) => EngineUtil.isWildcardField(kp2, index));
     const concretePath2 = kp2.slice(0, lastIndex2 + 1);
 
     if (lastIndex2 !== lastIndex1 || !concretePath1.equals(concretePath2))
