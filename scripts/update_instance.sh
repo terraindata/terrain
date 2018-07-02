55b290bdfd14ba165c82db67bf34e9615903290d
lukeknep <luke@knepper.com>
2018-07-02 10:55:33 -0700

    Changes to update instance to run staging on the same host on :7000

--- scripts/update_instance.sh
+++ scripts/update_instance.sh
@@ -2,9 +2,10 @@
 
 read -r -d '' HELPSTRING <<"EOF"
 Script to Update a Customer Instance.
-Usage: update_instance -n name -a address -[p]
+Usage: update_instance -n name -a address -v version-number -[p]
   -n    name of the customer
   -a    IP address of instance
+  -v    Release version number. e.g., 5.1
   -p    Is production and not staging
 EOF
 
@@ -16,8 +17,9 @@ if [ $# -eq 0 ]
 fi
 
 PREFIX="staging-"
+SCREEN_ID="7000"
 
-while getopts "n:a:p" opt; do
+while getopts "n:a:v:p" opt; do
   case $opt in
     n)
       CUSTNAME=$OPTARG
@@ -25,8 +27,12 @@ while getopts "n:a:p" opt; do
     a)
       ADDRESS=$OPTARG
       ;;
+    v)
+      VERSION=$OPTARG
+      ;;
     p)
       PREFIX=""
+      SCREEN_ID="3000"
       ;;
     \?)
       echo "Invalid option: -$OPTARG";
@@ -47,28 +53,34 @@ if [ -z "$ADDRESS" ]
     exit 1;
 fi
 
+if [ -z "$VERSION" ]
+  then
+    echo "Error: Version Not Supplied";
+    exit 1;
+fi
+
 MIDWAYHOSTNAME="${PREFIX}${CUSTNAME}.terraindata.com";
 
-echo "Beginning instance update with customer name \""$CUSTNAME"\" and address" $ADDRESS;
-echo "Using host name ${MIDWAYHOSTNAME}";
+echo "Beginning instance update with customer name \""$CUSTNAME"\" and IP address" $ADDRESS;
+echo "App will be running on: ${MIDWAYHOSTNAME}";
 
 echo "removing node modules...";
 
-rm -rf node_modules;
+# rm -rf node_modules;
 cd ..
 
 (! test -d Search) && (echo "Error: could not find Search directory"; exit 1);
 
 echo "rsyncing";
 
-rsync -vrP  --exclude midway.json --exclude midway.db --delete  $PWD/Search terrain@${ADDRESS}:src/ > "rsynclog.log";
+rsync -vrP  --exclude midway.json --exclude midway.db --exclude node_modules --delete  $PWD/Search terrain@${ADDRESS}:src-${VERSION}/ > "rsynclog.log";
 
 ssh terrain@${ADDRESS} << EOF
-cd src/Search
-screen -S runmidway -X quit;
-screen -d -m -S runmidway;
-screen -S runmidway -X stuff "yarn; MIDWAY_HOST=${MIDWAYHOSTNAME} yarn build-prod;";
-screen -S runmidway -X stuff "NODE_ENV=production yarn start-midway > >(tee -a /var/log/midway/midway.log) 2> >(tee -a /var/log/midway/midway_error.log >&2)";
+cd src-${VERSION}/Search
+screen -S runmidway-${SCREEN_ID} -X quit;
+screen -d -m -S runmidway-${SCREEN_ID};
+screen -S runmidway-${SCREEN_ID} -X stuff "yarn; yarn build-prod;";
+screen -S runmidway-${SCREEN_ID} -X stuff "NODE_ENV=production yarn start-midway > >(tee -a /var/log/midway/midway.log) 2> >(tee -a /var/log/midway/midway_error.log >&2)";
 EOF
 
 echo "End of Update Script";
