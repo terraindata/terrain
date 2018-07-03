5fb9dc36859ee278ea222a27e281cc0089f4ed06
lukeknep <luke@knepper.com>
2018-07-03 11:47:46 -0700

    New update script for v5 and beyond; includes same-host staging process and PG backup

--- scripts/update_instance.sh
+++ scripts/update_instance.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+# SECTION: Parse Input
+
 read -r -d '' HELPSTRING <<"EOF"
 Script to Update a Customer Instance.
 Usage: update_instance -n name -a address -v version-number -[p]
   -n    name of the customer
   -a    IP address of instance
   -v    Release version number. e.g., 5.1
-  -p    Is production and not staging
+  -p    (optional) Run the app in production (:3000) and not staging (:7000)
+  -s    (optional) Whether to make a copy of the current prod DB in a staging DB, and run that DB.
+  -d    (optional) Pass in the name of a DB to use when staging, instead of [customer]_production
 EOF
 
 if [ $# -eq 0 ]
@@ -16,10 +20,7 @@ if [ $# -eq 0 ]
     exit 1;
 fi
 
-PREFIX="staging-"
-SCREEN_ID="7000"
-
-while getopts "n:a:v:p" opt; do
+while getopts "n:a:v:sd:p" opt; do
   case $opt in
     n)
       CUSTNAME=$OPTARG
@@ -31,8 +32,13 @@ while getopts "n:a:v:p" opt; do
       VERSION=$OPTARG
       ;;
     p)
-      PREFIX=""
-      SCREEN_ID="3000"
+      PRODUCTION=true
+      ;;
+    d)
+      SOURCE_DB_NAME=$OPTARG
+      ;;
+    s)
+      STAGE_DB=true
       ;;
     \?)
       echo "Invalid option: -$OPTARG";
@@ -59,13 +65,80 @@ if [ -z "$VERSION" ]
     exit 1;
 fi
 
+
+
+# SECTION: Configure Settings
+
+if [ -z "$PRODUCTION" ]
+  then
+    MIDWAY_DB="${CUSTNAME}_staging"
+    PREFIX="staging-"
+    SCREEN_ID="7000"
+    PORT="7000"
+    echo "Deploying to staging instance."
+  else
+    MIDWAY_DB="${CUSTNAME}_production"
+    PREFIX=""
+    SCREEN_ID="3000"
+    PORT="3000"
+    echo "Deploying to production instance."
+fi
+
 MIDWAYHOSTNAME="${PREFIX}${CUSTNAME}.terraindata.com";
+
+
+
+# SECTION: Stage DB
+
+STAGE_DB_COMMAND=""
+if [ "$STAGE_DB" ]
+  then
+    if [ "$PRODUCTION" ]
+      then 
+        echo "ERROR: Do not stage the DB when deploying to production; otherwise, you will overwrite the prod DB."
+        exit 1;
+    fi
+    
+    if [ -z "$SOURCE_DB_NAME" ]
+      then
+        SOURCE_DB_NAME="${CUSTNAME}_production"
+    fi
+    
+    echo "Will stage DB: from ${SOURCE_DB_NAME} to ${MIDWAY_DB}"
+
+    # SECTION: Migration Scripts
+    # Set up which scripts to run on migrations.
+    # Devs: Add references to necessary verion upgrade migrations in this file.
+    # Note: These will only run if you are staging the DB.
+    # If you are deploying straight to production, and need a migration, you will
+    #  need to do that yourself.
+
+    MIGRATION_COMMAND=""
+    if [ "$VERSION"="5" ]
+      then
+        MIGRATION_COMMAND="psql -U 't3rr41n-demo' -d ${MIDWAY_DB} -h localhost -f scripts/MIDWAY_PG_MIGRATION.sql"
+        echo "Version 5 migrations applied."
+      else
+        echo "No migrations needed."
+    fi
+
+    
+    read -r -d '' STAGE_DB_COMMAND << EOM
+      export PGPASSWORD="r3curs1v3$"
+      psql -U "t3rr41n-demo" -d postgres -h localhost -p 5432 -c "drop database ${MIDWAY_DB};"
+      psql -U "t3rr41n-demo" -d postgres -h localhost -p 5432 -c "create database ${MIDWAY_DB};"
+      pg_dump -U "t3rr41n-demo" -d "${SOURCE_DB_NAME}" -h localhost -p 5432 | psql -U "t3rr41n-demo" -d "${MIDWAY_DB}" -h localhost -p 5432 
+      ${MIGRATION_COMMAND}
+EOM
+fi
+
+
+# SECTION: Version 
 
 echo "Beginning instance update with customer name \""$CUSTNAME"\" and IP address" $ADDRESS;
 echo "App will be running on: ${MIDWAYHOSTNAME}";
 
-echo "removing node modules...";
-
+# echo "removing node modules...";
 # rm -rf node_modules;
 cd ..
 
@@ -73,14 +146,15 @@ cd ..
 
 echo "rsyncing";
 
-rsync -vrP  --exclude midway.json --exclude midway.db --exclude node_modules --delete  $PWD/Search terrain@${ADDRESS}:src-${VERSION}/ > "rsynclog.log";
+# rsync -vrP  --exclude midway.json --exclude midway.db --exclude node_modules --delete  $PWD/Search terrain@${ADDRESS}:src-${VERSION}/ > "rsynclog.log";
 
 ssh terrain@${ADDRESS} << EOF
 cd src-${VERSION}/Search
 screen -S runmidway-${SCREEN_ID} -X quit;
+${STAGE_DB_COMMAND};
 screen -d -m -S runmidway-${SCREEN_ID};
 screen -S runmidway-${SCREEN_ID} -X stuff "yarn; yarn build-prod;";
-screen -S runmidway-${SCREEN_ID} -X stuff "NODE_ENV=production yarn start-midway > >(tee -a /var/log/midway/midway.log) 2> >(tee -a /var/log/midway/midway_error.log >&2)";
+screen -S runmidway-${SCREEN_ID} -X stuff "NODE_ENV=production yarn start-midway -p ${PORT} -i ${MIDWAY_DB} > >(tee -a /var/log/midway/midway_${VERSION}_${PORT}.log) 2> >(tee -a /var/log/midway/midway_error_${VERSION}_${PORT}.log >&2)\r";
 EOF
 
 echo "End of Update Script";
