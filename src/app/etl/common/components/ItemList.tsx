aff3a0735b756a519f355a6dbba8b1e974aa482c
jmansor <juanpablo.mansor@gmail.com>
2018-07-03 18:53:05 -0300

    Added loading message support to ItemList. Changed ScheduleList to set a loading message.

--- src/app/etl/common/components/ItemList.tsx
+++ src/app/etl/common/components/ItemList.tsx
@@ -78,12 +78,19 @@ export interface Props<T>
   itemsName?: string;
   canCreate?: boolean;
   onCreate?: () => void;
+  loading?: boolean;
+  loadingMessage?: string;
 }
 
 const memoize = _.memoize;
 
 export class ItemList<T> extends TerrainComponent<Props<T>>
 {
+  public static defaultProps = {
+    loading: false,
+    loadingMessage: null,
+  };
+
   constructor(props)
   {
     super(props);
@@ -159,6 +166,31 @@ export class ItemList<T> extends TerrainComponent<Props<T>>
     );
   }
 
+  public getEmptyItemsListMessage()
+  {
+    const { loading, loadingMessage } = this.props;
+
+    let message = '';
+
+    if (loading)
+    {
+      if (loadingMessage !== null)
+      {
+        message = loadingMessage;
+      }
+      else
+      {
+        message = `Loading ${this.props.itemsName || 'item'}s...`;
+      }
+    }
+    else
+    {
+      message = `There aren't yet any ${this.props.itemsName || 'item'}s`;
+    }
+
+    return message;
+  }
+
   public render()
   {
     const { columnConfig, items, getMenuOptions } = this.props;
@@ -200,7 +232,7 @@ export class ItemList<T> extends TerrainComponent<Props<T>>
             items.map(this.renderRow).toList()
             :
             <div className='item-list-message'>
-              There aren't yet any {this.props.itemsName || 'item'}s
+              {this.getEmptyItemsListMessage()}
             </div>
         }
 
