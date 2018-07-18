1557a033bfdf4984bbdc7c1e73cea5e38ba65b8e
lbrouckman <laura.brouckman@gmail.com>
2018-07-18 09:42:44 -0700

    in progress

--- src/app/util/TransformQueryUtil.ts
+++ src/app/util/TransformQueryUtil.ts
@@ -122,13 +122,13 @@ export const TransformQueryUtil = {
         let path = query.path;
         // Set outer size to 1000 and inner size to 1000
         path = path
-          .setIn(['source', 'count'], 1000)
+          .setIn(['source', 'count'], 10)
           .setIn(['more', 'source'], List([parentField]))
           .setIn(['more', 'customSource'], true)
           .setIn(['score', 'type'], 'random');
         path.nested.forEach((nested, i) => {
           path = path
-            .setIn(['nested', i, 'source', 'count'], 1000)
+            .setIn(['nested', i, 'source', 'count'], 10)
             .setIn(['nested', i, 'more', 'source'], List([input]))
             .setIn(['nested', i, 'more', 'customSource'], true)
             .setIn(['nested', i, 'score', 'type'], 'random');
