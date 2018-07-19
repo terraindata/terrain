e7eb5e829c1c1c5e07f4c2a623ba8257fe53cde2
lbrouckman <laura.brouckman@gmail.com>
2018-07-19 12:49:13 -0700

    Use .keyword when value is empty string

--- src/app/builder/components/pathfinder/PathfinderParser.ts
+++ src/app/builder/components/pathfinder/PathfinderParser.ts
@@ -616,6 +616,11 @@ function filterLineToQuery(line: FilterLine, indexPath, annotateQuery: boolean =
   const boost = typeof line.boost === 'string' ? parseFloat(line.boost) : line.boost;
   let query = {};
   let field = line.field;
+  // If the value is an empty string then set field to field.keyword
+  if (line.value === '' || line.value === null)
+  {
+    field = field + '.keyword';
+  }
   switch (line.comparison)
   {
     case 'notexists':
@@ -635,7 +640,7 @@ function filterLineToQuery(line: FilterLine, indexPath, annotateQuery: boolean =
         bool: {
           must_not: {
             term: {
-              [line.field]: {
+              [field]: {
                 value,
               },
             },
@@ -649,7 +654,7 @@ function filterLineToQuery(line: FilterLine, indexPath, annotateQuery: boolean =
         bool: {
           must_not: {
             match: {
-              [line.field]: {
+              [field]: {
                 query: value,
               },
             },
@@ -669,7 +674,7 @@ function filterLineToQuery(line: FilterLine, indexPath, annotateQuery: boolean =
     case 'equal':
       query = {
         term: {
-          [line.field]: {
+          [field]: {
             value,
             boost,
           },
@@ -679,7 +684,7 @@ function filterLineToQuery(line: FilterLine, indexPath, annotateQuery: boolean =
     case 'contains':
       query = {
         match: {
-          [line.field]: {
+          [field]: {
             query: value,
             boost,
           },
@@ -778,7 +783,8 @@ function filterLineToQuery(line: FilterLine, indexPath, annotateQuery: boolean =
       break;
     case 'isin':
       value = PathFinderStringToJSONArray(String(value));
-      field = line.fieldType === FieldType.Text && !line.analyzed ? field + '.keyword' : field;
+      field = line.fieldType === FieldType.Text && !line.analyzed && field.indexOf('.keyword') === -1
+            ? field + '.keyword' : field;
       query = {
         terms: {
           [field]: value,
@@ -788,12 +794,13 @@ function filterLineToQuery(line: FilterLine, indexPath, annotateQuery: boolean =
       break;
     case 'isnotin':
       value = PathFinderStringToJSONArray(String(value));
-      field = line.fieldType === FieldType.Text && !line.analyzed ? field + '.keyword' : field;
+      field = line.fieldType === FieldType.Text && !line.analyzed && field.indexOf('.keyword') === -1
+            ? field + '.keyword' : field;
       query = {
         bool: {
           must_not: {
             terms: {
-              [line.field]: value,
+              [field]: value,
             },
           },
           boost,
